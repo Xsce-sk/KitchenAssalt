21fae2a00e5e2d15568db70b0d626c082fe5effb
cjabs <cjabs97@gmail.com>
2019-02-23 19:39:07 -0800

    Change Jump Script
    - Changed the Physics2DSettings to disable starting queries in the collider
    - Added a player prefab
    - Changed the jump script to add more options to help properly detect ground

--- Assets/Scripts/Jumper.cs
+++ Assets/Scripts/Jumper.cs
@@ -4,63 +4,61 @@ using UnityEngine;
 
 public class Jumper : MonoBehaviour
 {
-    protected Rigidbody2D m_Rigidbody2D;
-    protected Transform m_Transform;
-    public float offset;
-    public float jumpForce = 50;
-    public int maxJumps = 1;
-    [SerializeField]
-    private int remainingJumps;
-    public float terminalVelocity = -5;
-    private bool isGrounded = false;
+    [Header("Jump Settings")]
+    public float jumpForce;
+    public int maxJumps;
+    public float terminalVelocity;
 
-    // Start is called before the first frame update
-    void Start()
+    [Header("Grounded Detection")]
+    public Vector2 offset;
+    public float objectWidth;
+    public float raycastLength;
+    public bool debug;
+
+    [SerializeField]
+    private int m_RemainingJumps;
+    private bool m_IsGrounded = false;
+
+    protected Transform m_Transform;
+    protected Rigidbody2D m_Rigidbody2D;
+
+    #region Public Functions
+
+    public void Jump()
     {
-        m_Rigidbody2D = this.gameObject.GetComponent<Rigidbody2D>();
-        m_Transform = this.gameObject.transform;
-        remainingJumps = maxJumps;
+        if (m_RemainingJumps > 0)
+        {
+            m_Rigidbody2D.velocity = new Vector2(m_Rigidbody2D.velocity.x, 0);
+            m_Rigidbody2D.AddForce(Vector2.up * jumpForce);
+            --m_RemainingJumps;
+        }
     }
 
-    void Update()
+    public bool IsGrounded()
+    {
+        return m_IsGrounded;
+    }
+
+    #endregion
+
+    private void Start()
+    {
+        m_Transform = this.gameObject.transform;
+        m_Rigidbody2D = this.gameObject.GetComponent<Rigidbody2D>();
+
+        m_RemainingJumps = maxJumps;
+    }
+
+    private void Update()
     {
         TerminalCheck();
     }
 
     private void TerminalCheck()
     {
-        if(m_Rigidbody2D.velocity.y < 0 && m_Rigidbody2D.velocity.y < terminalVelocity)
+        if(m_Rigidbody2D.velocity.y < -terminalVelocity)
         {
-            m_Rigidbody2D.velocity = new Vector2(m_Rigidbody2D.velocity.x, terminalVelocity);
-        }
-    }
-
-    public void Jump()
-    {
-        if(remainingJumps > 0)
-        {
-            m_Rigidbody2D.velocity = new Vector2(m_Rigidbody2D.velocity.x, 0);
-            m_Rigidbody2D.AddForce(Vector2.up * jumpForce);
-            --remainingJumps;
-        }
-    }
-
-    private void GroundCheck()
-    {
-        Vector2 origin = new Vector2(m_Transform.position.x, m_Transform.position.y - offset);
-        Debug.DrawRay(origin, Vector2.down * 0.1f, Color.green);
-        RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.down, 0.05f);
-
-        if (hit && hit.collider.CompareTag("Ground"))
-        {
-            remainingJumps = maxJumps;
-            isGrounded = true;
-        }
-        else
-        {
-            isGrounded = false;
-            if (remainingJumps == maxJumps)
-                --remainingJumps;
+            m_Rigidbody2D.velocity = new Vector2(m_Rigidbody2D.velocity.x, -terminalVelocity);
         }
     }
 
@@ -68,13 +66,65 @@ public class Jumper : MonoBehaviour
     {
         GroundCheck();
     }
+
     private void OnCollisionExit2D(Collision2D collision)
     {
-        GroundCheck();
+        if (collision.collider.CompareTag("Ground"))
+        {
+            m_IsGrounded = false;
+            if (m_RemainingJumps == maxJumps)
+                --m_RemainingJumps;
+        }
     }
 
-    public bool IsGrounded()
+    private void GroundCheck()
     {
-        return isGrounded;
+        RaycastHit2D[] collisions = new RaycastHit2D[3];
+
+        Vector2 middleOrigin = new Vector2(m_Transform.position.x + offset.x, m_Transform.position.y + offset.y);
+        Vector2 leftOrigin = new Vector2(middleOrigin.x - (objectWidth / 2), middleOrigin.y);
+        Vector2 rightOrigin = new Vector2(middleOrigin.x + (objectWidth / 2), middleOrigin.y);
+
+        collisions[0] = Physics2D.Raycast(middleOrigin, Vector2.down, raycastLength);
+        collisions[1] = Physics2D.Raycast(leftOrigin, Vector2.down, raycastLength);
+        collisions[2] = Physics2D.Raycast(rightOrigin, Vector2.down, raycastLength);
+        
+        foreach (RaycastHit2D hit in collisions)
+        {
+            if (hit && hit.collider.CompareTag("Ground"))
+            {
+                print(hit.collider.gameObject.name);
+                m_RemainingJumps = maxJumps;
+                m_IsGrounded = true;
+            }
+        }
+
+        /*
+        Vector2 middleOrigin = new Vector2(m_Transform.position.x + offset.x, m_Transform.position.y + offset.y);
+        RaycastHit2D hit = Physics2D.Raycast(middleOrigin, Vector2.down, raycastLength);
+
+        if (hit && hit.collider.CompareTag("Ground"))
+        {
+            print(hit.collider.gameObject.name);
+            m_RemainingJumps = maxJumps;
+            m_IsGrounded = true;
+        }
+        else
+        {
+            m_IsGrounded = false;
+            if (m_RemainingJumps == maxJumps)
+                --m_RemainingJumps;
+        }
+        */
+        
+        
+
+
+        if (debug)
+        {
+            Debug.DrawRay(middleOrigin, Vector3.down * raycastLength, Color.blue, 1);
+            Debug.DrawRay(leftOrigin, Vector3.down * raycastLength, Color.blue, 1);
+            Debug.DrawRay(rightOrigin, Vector3.down * raycastLength, Color.blue, 1);
+        }
     }
 }
