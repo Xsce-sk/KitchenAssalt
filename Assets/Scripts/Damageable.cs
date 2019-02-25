f32eb5be616fab121e2d24e6e20e00732b5e96f2
wiwinewijaya <wiwinwijaya97@yahoo.com>
2019-02-24 19:17:57 -0800

    Added Damageable and Damager (Not Complete)

---
+++ Assets/Scripts/Damageable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damageable : MonoBehaviour
+{
+    public int m_MaxHealth = 3;
+    public int m_CurrentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_CurrentHealth = m_MaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(m_CurrentHealth);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        m_CurrentHealth -= damage;
+    }
+}
