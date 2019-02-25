5e413616bade4dafde032404729de29df747d4ec
rygreen <rygreen99@gmail.com>
2019-02-25 00:15:55 -0800

    Add Weapon Manager
    Public functions to select and fire current weapon, adding guns soon :^)

---
+++ Assets/Scripts/WeaponManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+	public List<GameObject> weapons;
+	private GameObject currWeapon;
+	private Shooter currShooter;
+	private int currIndex;
+
+	void Start()
+	{
+		currIndex = 0;
+	}
+
+	public void nextWeapon()
+	{
+		++currIndex;
+		if(currIndex == weapons.Count)
+			currIndex = 0;
+		changeWeapon();
+	}
+
+	public void previousWeapon()
+	{
+		++currIndex;
+		if(currIndex < 0)
+			currIndex = weapons.Count - 1;
+		changeWeapon();
+	}
+
+	public void explicitWeapon(int weaponIndex)
+	{
+		currIndex = weaponIndex;
+		changeWeapon();
+	}
+
+	public void shootWeapon()
+	{
+		currShooter.shoot();
+	}
+
+	private void changeWeapon()
+	{
+		if(currWeapon != null)
+			Destroy(currWeapon);
+		currWeapon = Instantiate(weapons[currIndex], transform);
+		currShooter = currWeapon.GetComponent<Shooter>();
+	}
+
+}
