b869fa80db15c7eb0e288b45e5067e3db845eee4
wiwinewijaya <wiwinwijaya97@yahoo.com>
2019-03-03 19:02:22 -0800

    Added player blinking
    Done by changing the alpha component of the color of the sprite renderer

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -8,10 +8,21 @@ public class PlayerScript : MonoBehaviour, IDamageable
 
     private int m_CurrentHealth;
 
+    public SpriteRenderer sr;
+
+    public float opacity = 0.5f;
+    public float blinkDuration = 0.25f;
+
+    private Color startColor;
+    private Color damagedColor;
+    private bool isDamagable = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_CurrentHealth = maxHealth;
+        startColor = sr.color;
+        damagedColor = new Color(startColor.r, startColor.g, startColor.b, opacity);
     }
 
     // Update is called once per frame
@@ -22,7 +33,15 @@ public class PlayerScript : MonoBehaviour, IDamageable
 
     public void LoseHealth(int healthDelta)
     {
-        m_CurrentHealth -= healthDelta;
+        if (isDamagable)
+        {
+            m_CurrentHealth -= healthDelta;
+
+            if (m_CurrentHealth > 0)
+            {
+                StartCoroutine("Blink");
+            }
+        }
 
         // Do something when we die/get hit by something.
     }
@@ -30,5 +49,14 @@ public class PlayerScript : MonoBehaviour, IDamageable
     public void Stun(float duration)
     {
         // For if we want to be able to stun the player.
+    }
+
+    IEnumerator Blink()
+    {
+        sr.color = damagedColor;
+        isDamagable = false;
+        yield return new WaitForSeconds(blinkDuration);
+        sr.color = startColor;
+        isDamagable = true;
     }
 }
