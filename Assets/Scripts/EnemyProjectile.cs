17508a9d4cab3a240a994a47799267c7fbe52d69
wiwinewijaya <wiwinwijaya97@yahoo.com>
2019-03-05 14:25:00 -0800

    Added EnemyGroundShooting and Projectile
    -Enemy Shoots when the player is in range and stops moving while doing so

---
+++ Assets/Scripts/EnemyProjectile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour
+{
+    public float speed;
+    public bool destroyOnBecomeInvisible;
+
+
+    //private float m_Direction;
+
+    protected Transform m_Transform;
+    protected Rigidbody2D m_RigidBody2D;
+    public Vector2 m_Direction;
+
+    void Start()
+    {
+        m_Transform = this.gameObject.transform;
+        m_RigidBody2D = GetComponent<Rigidbody2D>();
+
+        //m_Direction = m_Transform.localScale.x;
+        m_RigidBody2D.AddForce(m_Direction * speed, ForceMode2D.Impulse);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/GroundEnemyShooter.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundEnemyShooter : MonoBehaviour
+{
+    public Transform player;
+    public EnemyMovement m_Movement;
+    public GameObject projectileType;
+    private Transform m_Transform;
+    public float range = 5;
+    private bool m_CanShoot = true;
+    public float cooldownTime = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Transform = this.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsCloseToPlayer() && m_CanShoot)
+        {
+            Shoot();
+            StartCoroutine(Cooldown());
+        }
+    }
+
+    private bool IsCloseToPlayer()
+    {
+        if (Vector2.Distance(player.position, m_Transform.position) <= range)
+            return true;
+
+        return false;
+    }
+
+    public void Shoot()
+    {
+        Vector3 playerPos = player.position; ;
+        Vector3 dir = new Vector3(playerPos.x - m_Transform.position.x,
+                                  playerPos.y - m_Transform.position.y, 0);
+
+        //Debug.Log($"{v.normalized}  {v}");
+
+        Vector3 spawnPosition = m_Transform.position + dir.normalized;
+
+        GameObject newProjectile = Instantiate(projectileType, spawnPosition, Quaternion.identity) as GameObject;
+
+        newProjectile.GetComponent<EnemyProjectile>().m_Direction = dir.normalized;
+    }
+
+    IEnumerator Cooldown()
+    {
+        m_CanShoot = false;
+        float startSpeed = m_Movement.moveSpeed;
+        m_Movement.moveSpeed = 0;
+        yield return new WaitForSeconds(cooldownTime);
+        m_Movement.moveSpeed = startSpeed;
+        m_CanShoot = true;
+    }
+}
