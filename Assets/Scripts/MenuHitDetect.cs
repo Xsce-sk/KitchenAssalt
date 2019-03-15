1df9314cd8093d6c3ce4b8e7334ea44687c84722
Joshua Tanenbaum <tanenbaj@uci.edu>
2019-03-14 17:32:22 -0700

    Added MenuHitDetect

---
+++ Assets/Scripts/MenuHitDetect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuHitDetect : MonoBehaviour, IDamageable
+{
+    public bool quitButton = false;
+    public bool startButton = false;
+
+    public int sceneIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoseHealth(int healthDelta)
+    {
+        if (startButton)
+            GameController.LoadSceneByIndex(sceneIndex);
+        else if (quitButton)
+            Application.Quit();
+    }
+
+    public void Stun(float duration)
+    {
+        //Do the dew...Mountian Dew
+    }
+}
