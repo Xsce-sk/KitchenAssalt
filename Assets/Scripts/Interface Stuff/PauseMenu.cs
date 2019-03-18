daa96ebb01f5c041c18707dc48ea88ef03aedcd0
Jackson Greaves <jdgreave@uci.edu>
2019-03-18 16:38:45 -0700

    added dummy functions for pause menu

---
+++ Assets/Scripts/Interface Stuff/PauseMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public void ContGame()
+    {
+        // for continue button
+    }
+
+    public void QuitGame()
+    {
+        // for quit button
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
